b1d1aeb7a243f94b9257136ddfac8bc3e5ed3034
Jacky Price <jackyprice@rentalcars.com>
2021-01-03 14:44:44 +0000

    added controllers and tests illustrating the 3 main dependency injection types: parameter, setter and constructor

--- src/main/java/com/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/com/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -1,8 +1,12 @@
 package com.springframework.sfgdi.controllers;
 
+import com.springframework.sfgdi.services.GreetingService;
 
 public class PropertyInjectedController {
+
     public GreetingService greetingService;
 
-    public
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
 }
